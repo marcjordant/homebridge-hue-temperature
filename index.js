091d52e71d91b76db2a7b52722b0dfc7f25d3d27
Marc Jordant <marcjordant@gmail.com>
2017-06-02 17:55:39 +0200

    Simplification of the code

--- index.js
+++ index.js
@@ -16,132 +16,77 @@ function TemperatureAccessory(log, config) {
     this.hueBridgeIPAddress = config["hue-bridge-ip-address"];
     this.hueBridgeUsername = config["hue-bridge-username"];
     this.hueSensorId = config["hue-sensor-id"];
-    this.lastupdate = 0;
-    this.temperature = 0;
-    this.lastupdateBattery = 0;
-    this.battery = 0;
 }
 
 TemperatureAccessory.prototype =
+{
+    getState: function (callback)
     {
-        getState: function (callback) {
-            // Only fetch new data every 1 minute
-            if (this.lastupdate + (60 * 1) < (Date.now() / 1000 | 0)) {
-                var url = "http://" + this.hueBridgeIPAddress + "/api/" + this.hueBridgeUsername + "/sensors/" + this.hueSensorId;
-                
-                this.httpRequest(url, function (error, response, responseBody) {
-                    if (error) {
-                        this.log("HTTP get sensor status function failed: %s", error.message);
-                        callback(error);
-                    } else {
-                        var statusObj = JSON.parse(responseBody);
-                        var temperature = statusObj["state"]["temperature"];
-                        temperature = temperature.toString().substring(0, 2);
-                        this.temperature = parseInt(temperature);
-                        this.lastupdate = (Date.now() / 1000);
-                        callback(null, this.temperature);
-                    }
-                }.bind(this));
-            } else {
-                temperatureService.setCharacteristic(Characteristic.CurrentTemperature, this.temperature);
-                callback(null, this.temperature);
-            }
-        },
+      var url = "http://" + this.hueBridgeIPAddress + "/api/" + this.hueBridgeUsername + "/sensors/" + this.hueSensorId;
+      
+      this.httpRequest(url, function (error, response, responseBody) {
+          if (error) {
+              callback(error);
+          }
+          else {
+              var statusObj = JSON.parse(responseBody);
+              var temperatureObj = statusObj["state"]["temperature"];
+              var temperature = parseFloat(temperatureObj.toString().substring(0, 2) + "." + temperatureObj.toString().substring(2, 4));
+              callback(null, temperature);
+          }
+      });
+    },
 
-        getActive: function (callback) {
-                var url = "http://" + this.hueBridgeIPAddress + "/api/" + this.hueBridgeUsername + "/sensors/" + this.hueSensorId;
-                
-                this.httpRequest(url, function (error, response, responseBody) {
-                    if (error) {
-                        this.log("HTTP get sensor status function failed: %s", error.message);
-                        callback(null, false);
-                    } else {
-                        callback(null, true);
-                    }
-                }.bind(this));
-        },
-
-        getBatteryLevel: function (callback) {
-            // Only fetch new data every 1 minute
-            if (this.lastupdateBattery + (60 * 1) < (Date.now() / 1000 | 0)) {
-                var url = "http://" + this.hueBridgeIPAddress + "/api/" + this.hueBridgeUsername + "/sensors/" + this.hueSensorId;
-                
-                this.httpRequest(url, function (error, response, responseBody) {
-                    if (error) {
-                        this.log("HTTP get sensor status function failed: %s", error.message);
-                        callback(error);
-                    } else {
-                        var statusObj = JSON.parse(responseBody);
-                        var battery = parseInt(statusObj["config"]["battery"]);
-                        this.battery = battery <= 20 ? true : false;
-                        this.lastupdateBattery = (Date.now() / 1000);
-                        callback(null, battery);
-                    }
-                }.bind(this));
-            } else {
-                temperatureService.setCharacteristic(Characteristic.StatusLowBattery, this.battery);
-                callback(null, this.battery);
-            }
-        },
+    getBatteryLevel: function (callback)
+    {
+      var url = "http://" + this.hueBridgeIPAddress + "/api/" + this.hueBridgeUsername + "/sensors/" + this.hueSensorId;
+      
+      this.httpRequest(url, function (error, response, responseBody) {
+          if (error) {
+              callback(error);
+          } else {
+              var statusObj = JSON.parse(responseBody);
+              var batteryLevel = parseInt(statusObj["config"]["battery"]);
+              var battery = batteryLevel <= 20 ? true : false;
+              callback(null, battery);
+          }
+      });
+    },
 
 
+    getServices: function ()
+    {
+        var informationService = new Service.AccessoryInformation();
 
-        identify: function (callback) {
-            this.log("Identify requested");
-            callback();
-        },
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, "singleTapps")
+            .setCharacteristic(Characteristic.Model, "HueTempSensor")
+            .setCharacteristic(Characteristic.SerialNumber, "1GWZ009");
 
+        temperatureService = new Service.TemperatureSensor(this.name);
+        temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .on('get', this.getState.bind(this));
 
+        temperatureService
+            .getCharacteristic(Characteristic.StatusLowBattery)
+            .on('get', this.getBatteryLevel.bind(this));
 
-        getServices: function () {
-            var informationService = new Service.AccessoryInformation();
+        return [informationService, temperatureService];
+    },
 
-            informationService
-                .setCharacteristic(Characteristic.Manufacturer, "singleTapps")
-                .setCharacteristic(Characteristic.Model, "Hue Temperature sensor")
-                .setCharacteristic(Characteristic.SerialNumber, "1GWZ009");
-
-            temperatureService = new Service.TemperatureSensor(this.name);
-            temperatureService
-                .getCharacteristic(Characteristic.CurrentTemperature)
-                .on("get", this.getState.bind(this));
-
-            temperatureService
-                .getCharacteristic(Characteristic.CurrentTemperature)
-                .setProps({minValue: -30});
-
-            temperatureService
-                .getCharacteristic(Characteristic.CurrentTemperature)
-                .setProps({maxValue: 120});
-
-            temperatureService
-                .getCharacteristic(Characteristic.StatusActive)
-                .on("get", this.getActive.bind(this));
-
-            temperatureService
-                .getCharacteristic(Characteristic.StatusLowBattery)
-                .on("get", this.getBatteryLevel.bind(this));
-
-
-            return [informationService, temperatureService];
-        },
-
-        httpRequest: function (url, callback) {
-            request({
-                    url: url,
-                    body: "",
-                    method: "GET",
-                    rejectUnauthorized: false
-                },
-                function (error, response, body) {
-                    callback(error, response, body)
-                })
-        }
-
-    };
-
-if (!Date.now) {
-    Date.now = function () {
-        return new Date().getTime();
+    httpRequest: function (url, callback) 
+    {
+        request({
+                url: url,
+                body: "",
+                method: "GET",
+                rejectUnauthorized: false
+            },
+            function (error, response, body) {
+                callback(error, response, body)
+            })
     }
-}
+
+};
+
